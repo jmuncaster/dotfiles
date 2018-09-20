90ef4ac1c921cd3f1c4db403c6219e4971f781fe
Justin Muncaster <justin@muncaster.io>
2018-09-19 22:56:14 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+set -v
+
+# Download/clone plugins
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+bash $DIR/scripts/download.sh
+
+# Copy update script
+cp $DIR/scripts/update.sh ~/.vim/bundle
+
+# Update .vimrc
+cp $DIR/dotvimrc ~/.vimrc
+
