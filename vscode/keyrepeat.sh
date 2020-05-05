e620087f8b63c966219df3b912c02c0e3be4e769
Justin Muncaster <jmuncaster@muncaster.io>
2020-05-04 17:36:20 -0700

    Add fixer for vscode

---
+++ vscode/keyrepeat.sh
@@ -0,0 +1,5 @@
+echo "Changing defaults according to: https://github.com/VSCodeVim/Vim#mac-setup"
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
+
